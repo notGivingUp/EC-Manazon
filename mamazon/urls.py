e539b0fdc91be3c92b2871b6c426eb705213b413
hxduc <hoxuanduc172@gmail.com>
2021-05-11 18:07:44 +0900

    検索機能を追加

--- mamazon/urls.py
+++ mamazon/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'mamazon'
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home'),
+    path('products/', views.ProductListView.as_view(), name='product-list')
 ]
